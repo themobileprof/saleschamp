2b02ed85fa5b60588db0a4e2b136b6d3d1133ad4
Samuel <samuelanyaele@gmail.com>
2021-05-28 22:21:17 +0100

    Working terraform and install.sh

--- terraform/install.sh
+++ terraform/install.sh
@@ -1,67 +1,74 @@
 #!/bin/bash
 
 # Setup Updates
-apt-get update -y
+sudo apt update -y
 
 # Install Curl for the next action 
-apt install curl git nginx -y
-
-# Configure Git
-git config --global user.name "saleschamp"
-git config --global user.email samuelanyaele@gmail.com
-git config --global pull.rebase false
-
-# Download and run nvm installer
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-
-# Load NVM
-. ~/.nvm/nvm.sh
-
-
-# Install LTS Version of Node
-nvm install node
-
-mkdir ~/sites
-cd ~/sites
-
-# Clone the SalesChamp repo
-git clone https://github.com/themobileprof/saleschamp.git ~/sites/saleschamp
-
-# Install PM
-npm install pm2@latest -g
-
-
-# Run The NodeJS App in the background
-pm2 start ~/sites/saleschamp/app.js
-pm2 save
+sudo apt install curl git nginx -y
 
 # Setup Nginx and SSH to allow on firewall
-ufw allow 443/tcp
-ufw allow 80/tcp
-ufw allow 5000/tcp
-ufw allow ssh
+sudo ufw allow 443/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 5000/tcp
+sudo ufw allow ssh
 
 
 # Configure Nginx
-cat <<EOF > /etc/nginx/sites-available/saleschamp
+sudo cat <<EOF > /etc/nginx/sites-available/default
 server {
 	listen 80 default_server;
 
 	location / {
         proxy_pass http://localhost:5000;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection 'upgrade';
-        proxy_set_header Host $host;
-        proxy_cache_bypass $http_upgrade;
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
     }
 }
 EOF
 
-# Enable website
-ln -s /etc/nginx/sites-available/saleschamp /etc/nginx/sites-enabled/
-
 
 # Restart Nginx
-systemctl restart nginx
+sudo systemctl restart nginx
+
+# Download and run Nodejs LTS installer
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt install -y nodejs
+
+# Install PM
+npm install pm2@latest -g
+pm2 startup
+
+
+# Go to the www directory
+cd /var/www/html
+
+# Clone the SalesChamp repo
+git clone https://github.com/themobileprof/saleschamp.git /var/www/html/saleschamp
+
+
+# Create a PM2 Group
+groupadd pm2
+
+# Change www directorygroup owner to group pm2
+chgrp -R pm2 /var/www/html/saleschamp
+
+# Add "ubuntu" to pm2 group
+usermod -aG pm2 ubuntu
+
+# Create an Alias
+echo "alias pm2='env HOME=/var/www pm2'" > /etc/profile.d/00-pm2.sh
+
+
+
+
+
+# Go to the saleschamp directory
+cd /var/www/html/saleschamp
+
+# Run The NodeJS App in the background
+pm2 start app.js
+pm2 save
 
